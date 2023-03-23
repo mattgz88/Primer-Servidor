2e493f46dc2942cb60d4fc8c10583553c3f501ea
matias <d46587536@alumnos.itr.edu.ar>
2023-03-23 10:16:28 -0300

    segundo commit

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express')
-const app = express()
+const morgan = require('morgan')
+const ejs = require('ejs')
+const path = require('path')
+const bodyparser = require('body-parser')
+const app = express() 
 const port = 2000
 
-app.get('/petit', (req, res) => {
-  res.send('Que bendiciooooon!')
+//MIDDLEWARES
+//app.use(morgan(dev));
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+app.use(express.static('/public'));
+app.set("view engine", "ejs");
+
+app.post('/', (req, res) => {
+  res.render('petit', req.body);
+A});
+
+aqqz<ºpp.get('/a', (req, res) => {
+  res.render("petit", { name: 'Tu Nombre', title: 'Título de la Página', age: 31 })
 })
 
 app.listen(port, () => {
